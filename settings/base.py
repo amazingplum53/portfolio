66a62a2b6b8c88c7df3230b0090ef498c8fcc057
amazingplum53 <103386225+amazingplum53@users.noreply.github.com>
2023-03-05 12:03:38 +0000

    media

--- settings/base.py
+++ settings/base.py
@@ -112,6 +112,8 @@ STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
 
+MEDIA_URL = '/media/'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
