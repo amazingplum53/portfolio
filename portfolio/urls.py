3b29c2f691ecf80697776abc15ffd7ddce6e0a10
amazingplum53 <103386225+amazingplum53@users.noreply.github.com>
2023-03-17 16:16:41 +0000

    new app

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('homepage/', include('homepage.urls')),
-    path('', include('homepage.urls')),
+    path('blog/', include('blog.urls')),
 ]
