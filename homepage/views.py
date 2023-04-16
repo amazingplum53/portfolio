c4d3e12449c222e3b4acf4ba1617a1e8018303ff
amazingplum53 <103386225+amazingplum53@users.noreply.github.com>
2023-04-16 18:04:36 +0100

    email setup

--- homepage/views.py
+++ homepage/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render #HttpResponse
+from django.shortcuts import render, HttpResponseRedirect #HttpResponse
+from django.core.mail import EmailMessage
 
 from .forms import ContactForm
 
@@ -8,16 +9,36 @@ def home(request):
     return render(
         request,
         'index.html',
-        {"contact_form": ContactForm}
+        {"contact_form": ContactForm()}
     )
 
 
 def contact(request):
 
+    if request.method == "POST":
+
+        try:
+
+            message = EmailMessage(
+                'Subject here',
+                'Here is the message.',
+                'support@matthewhill.click',
+                ['matthewpaulh@hotmail'],
+            )
+
+            message.send()
+
+        except Exception as e:
+            
+            raise e
+
+    else:
+
+        return HttpResponseRedirect("/homepage/about/")
+
     return render(
         request,
         'contact.html',
-        {"contact_form": ContactForm}
     )
 
 
@@ -26,5 +47,5 @@ def projects(request):
     return render(
         request,
         'projects.html',
-        {"contact_form": ContactForm}
+        {"contact_form": ContactForm()}
     )
