ce88159e554596588fcd42a634668e04460a7e33
amazingplum53 <103386225+amazingplum53@users.noreply.github.com>
2023-06-09 22:20:45 +0100

    emails

--- homepage/views.py
+++ homepage/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, HttpResponseRedirect #HttpResponse
-from django.core.mail import EmailMessage
-
+from portfolio.send import send_email
+from django.conf import settings
+from django.template.loader import render_to_string
 from .forms import ContactForm
 
 
@@ -12,25 +13,48 @@ def home(request):
         {"contact_form": ContactForm()}
     )
 
+"""
+from django.core.mail import send_mail
+
+if form.is_valid():
+    subject = form.cleaned_data["subject"]
+    message = form.cleaned_data["message"]
+    sender = form.cleaned_data["sender"]
+    cc_myself = form.cleaned_data["cc_myself"]
+
+    recipients = ["info@example.com"]
+    if cc_myself:
+        recipients.append(sender)
+
+    send_mail(subject, message, sender, recipients)
+    return HttpResponseRedirect("/thanks/")
+"""
 
 def contact(request):
 
     if request.method == "POST":
 
-        try:
+        form = ContactForm(request.POST)
 
-            message = EmailMessage(
-                'Subject here',
-                'Here is the message.',
-                'support@matthewhill.click',
-                ['matthewpaulh@hotmail'],
+        if form.is_valid():
+
+            html_content = render_to_string(
+                "contact_email.html", 
+                {
+                    "sender": form.cleaned_data["sender"],
+                    "message": form.cleaned_data["message"]
+                }
+            )
+
+            message = send_email(
+                ["matthewpaulh@hotmail.co.uk"],
+                form.cleaned_data["subject"],
+                "contact" + settings.BASE_EMAIL_SENDER,
+                html_content,
+                cc = form.cleaned_data["sender"] if form.cleaned_data["cc_myself"] else []
             )
 
             message.send()
-
-        except Exception as e:
-            
-            raise e
 
     else:
 
---
+++ portfolio/send.py
@@ -0,0 +1,75 @@
+from os import environ
+from mailjet_rest import Client
+from django.conf import settings
+from base64 import b64encode
+
+
+def file_encode(file_path):
+
+    with open(file_path, "rb") as f:
+
+        return b64encode(f.read())
+    
+
+def send_email(
+    to, subject, _from = "portfolio" + settings.BASE_EMAIL_SENDER, html_content = " ",
+    cc = [], bcc = [], attachments = []
+):
+
+    recipient = "matthewpaulh@hotmail.co.uk"
+
+    app_key = environ["email application key"]
+
+    secret_key = environ["email secret key"]
+
+    email_client = Client(auth = (app_key, secret_key), version = 'v3.1')
+
+    data = {'Messages': []}
+
+    for recipient in to:
+            
+        data["Messages"].append(
+            {
+                "From": {
+                    "Email": _from,
+                    "Name": "Matthew Hill"
+                },
+
+                "To": [
+                    {
+                        "Email": recipient,
+                        "Name": "You"
+                    }
+                ],
+
+                "Cc": [
+                    {
+                        "Email": contact,
+                    } for contact in cc
+                ],
+
+                "Bcc": [
+                    {
+                        "Email": contact,
+                    } for contact in bcc
+                ],
+
+                "Attachments": [
+                    {
+                        "ContentType": a["ContentType"],
+                        "Filename": a["Filename"],
+                        "Base64Content": file_encode(a["Filepath"])
+                    } for a in attachments
+                ],
+
+                "Subject": subject,
+                "TextPart": "",
+                "HTMLPart": html_content
+            }
+        )
+
+    if len(data["Messages"]) > 0:
+
+        result = email_client.send.create(data=data)
+        print(result.status_code)
+        return result.json()
