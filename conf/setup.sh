1ba43226fde923191b3ff6dd445668e25e06cc93
amazingplum53 <103386225+amazingplum53@users.noreply.github.com>
2024-03-16 17:03:07 +0000

    not needed

--- conf/setup.sh
+++ conf/setup.sh
@@ -8,8 +8,6 @@ cd $BASEDIR
 
 /usr/bin/git pull
 
-/usr/bin/systemctl start supervisor
-
 /usr/bin/openssl req -x509 -newkey rsa:4096 -keyout $VOLUMEDIR/key.pem -out $VOLUMEDIR/cert.pem -sha256 -days 365 -config $BASEDIR/conf/openssl.cnf -nodes
 
 /usr/sbin/nginx
