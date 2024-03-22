41b557885f3a2c0d3d37f57a8878f8795bbb3023
amazingplum53 <103386225+amazingplum53@users.noreply.github.com>
2024-03-22 21:47:16 +0000

    changed loading point of ssl

--- conf/gunicorn.config.py
+++ conf/gunicorn.config.py
@@ -4,6 +4,7 @@ from multiprocessing import cpu_count
 from os import environ
 import subprocess
 from portfolio.env import get_environ_variables, generate
+from ..settings.base import BASE_DIR, VOLUME_DIR
 
 
 max_workers = cpu_count
@@ -29,6 +30,21 @@ def on_starting(server):
     except:
         generate(".")
         get_environ_variables(".")
+
+    if environ["SETTINGS"] in ["test", "live"]:
+        pass
+    else:
+
+        #/usr/bin/openssl req -x509 -newkey rsa:4096 -keyout $VOLUMEDIR/key.pem -out $VOLUMEDIR/cert.pem -sha256 -days 365 -config $BASEDIR/conf/openssl.cnf -nodes
+
+        subprocess.run([
+            "/usr/bin/openssl", "req", "-x509", "-newkey", "rsa:4096", 
+            "-keyout", f"{VOLUME_DIR}/key.pem",
+            "-out", f"{VOLUME_DIR}/cert.pem", 
+            "-sha256", "-days", "365", 
+            "-config", f"{BASE_DIR}/conf/openssl.cnf", 
+            "-nodes"
+        ])  
 
 
 def when_ready(server):
--- settings/local.py
+++ settings/local.py
@@ -15,16 +15,5 @@ DEBUG = True
 ALLOWED_HOSTS = [
     '127.0.0.1',
     'localhost'
-]
-
-#/usr/bin/openssl req -x509 -newkey rsa:4096 -keyout $VOLUMEDIR/key.pem -out $VOLUMEDIR/cert.pem -sha256 -days 365 -config $BASEDIR/conf/openssl.cnf -nodes
-
-subprocess.run([
-    "/usr/bin/openssl", "req", "-x509", "-newkey", "rsa:4096", 
-    "-keyout", f"{VOLUME_DIR}/key.pem",
-    "-out", f"{VOLUME_DIR}/cert.pem", 
-    "-sha256", "-days", "365", 
-    "-config", f"{BASE_DIR}/conf/openssl.cnf", 
-    "-nodes"
-])              
+]            
 
